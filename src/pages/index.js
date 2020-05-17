8bdf486477b4e5d662e33c9ff366073ac43a3c33
Finn Thye <piqueen@gmail.com>
2020-05-17 11:08:33 -0600

    First commit of default starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>WWC Career Returnship</h1>
+    <p>Ignite your tech trajectory through community</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
