d1ab066181e15e3f56f68855229ba12950f09fdc
Finn Thye <piqueen@gmail.com>
2020-07-01 12:30:24 -0600

    Put about and logo sections in one div for 100vh

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import React from "react"
-import { PageProps, Link } from "gatsby"
 import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import icon from "../images/WWC-icon.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Wrapper>
-      <div className="career-returnship">
-        <img />
-        <h1>CAREER RETURNSHIP</h1>
-      </div>
-      <div className="about-us"></div>
+      <div className="landing">
+        <div className="career-returnship">
+          <img src={icon}/>
+          <h1>CAREER RETURNSHIP</h1>
+        </div>
+        <div className="about-us">
+          <h2>ABOUT US</h2>
+        </div>
+      </div>  
       <div className="get-involved"></div>
       <div className="events"></div>
       <div className="projects"></div>
@@ -29,28 +33,56 @@ const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
 
-  .career-returnship {
+  .landing {
     display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 570px;
-    width: 100%;
-    background: linear-gradient(0deg, #4C4C4C, #4C4C4C), url(Hero-img.jpg);
-    background-blend-mode: multiply, normal;
+    flex-direction: column;
+    min-height: 100vh;
 
-    
-    img {
-      height: 120px;
-      margin-right: 5px;
+    .career-returnship {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-height: 50vh;
+      width: 100%;
+      background: linear-gradient(0deg, #4C4C4C, #4C4C4C), url(Hero-img.jpg);
+      background-blend-mode: multiply, normal;
+
+      img {
+        height: 120px;
+        margin-right: 5px;
+      }
+
+      h1 {
+        font-family: Montserrat, sans-serif;
+        font-size: 2rem;
+        font-weight: lighter;
+        letter-spacing: 0.02em;
+        color: #FFFFFF;
+      }
     }
 
-    h1 {
-      font-family: "Montserrat";
-      font-size: 2rem;
-      font-weight: normal;
-      letter-spacing: 0.02em;
-      color: #FFFFFF;
+    .about-us {
+
+      h2 {
+        font-family: Myriad Pro, sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 42px;
+        letter-spacing: 0.02em;
+      }
     }
+  }
+
+  .get-involved {
+
+  }
+
+  .events {
+
+  }
+
+  .projects {
+
   }
 `
 
