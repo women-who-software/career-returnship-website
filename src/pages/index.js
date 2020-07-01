8569fdb3936b39771447c688c1d53954c545f7d5
Rafna <rafnabash@gmail.com>
2020-06-30 19:07:31 -0600

    Revert "Merge pull request #26 from wwcodecolorado/15-experimental-home-page"
    This reverts commit 67824fa200f341bd162c0325859196550a668a02, reversing
    changes made to 5ae5b21c3e10f3a2c6768500c100ef783175c198.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,25 @@
 import React from "react"
-import { PageProps, Link } from "gatsby"
-import styled from "styled-components"
+import { Link } from "gatsby"
+
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = (props: PageProps) => (
+const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Wrapper>
-
-    </Wrapper>
+    <h1>Boulder/Denver Women Who Code Tech Returnship Program</h1>
+    <p>Ignite your tech trajectory through community</p>
+    <p>Now let's build something great together.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/about/">About Us</Link>
+    <br />
+    <Link to="/announcements/">Announcements</Link>
+    <br />
+    <Link to="/projects/">Projects</Link>
   </Layout>
 )
-
-const Wrapper = styled.div`
-  align-items: center;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  /* max-width: ; */
-  padding: 100px 0px 55px;
-  width: 100%;
-`
 
 export default IndexPage
