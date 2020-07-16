2959c6ac5e0ff03bc6eab0285d7423cb5b412fa6
Rafna <rafnabash@gmail.com>
2020-07-16 15:09:27 -0600

    added figma componenets to the home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,22 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    /*{
+      resolve: `gatsby-source-figma`,
+      options: {
+        // For files:
+        fileId: `MbMj1N23bQuMvZwoptLK7N`,
+        // For images:
+        nodeIds: [`1%3A2`],
+        // optional for nodeIds: A number between 0.01 and 4, the image scaling factor
+        scale: 1,
+        // optional: A string enum for the image output format, can be jpg, png, svg, or pdf
+        format: 'png',
+        // For projects:
+        //projectId: `834958753448044552`,
+        // Get an access token from Figma Account Settings.
+        accessToken: `52695-b180b61c-30bf-4e98-8830-9f65c88dd071`,
+      },
+    },*/
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,36 +11,44 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Img from "gatsby-image"
 
-const Layout = ({ children }) => {
+const Layout = () => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+      file(relativePath: { eq: "Hero-subtract.png" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
+    }`
+    /*query SiteTitleQuery {
+      site {
+      siteMetadata {
+        title
+      }
     }
-  `)
+    /*
+    const data = graphql`
+    query ImageQuery {
+  allFigmaImage {
+    nodes {
+      id
+      image
+    }
+  }
+}`*/
+  )
+//{/*{data.site.siteMetadata.title}*/}
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div>
+      <Img fluid={data.file.childImageSharp.fluid}
+          alt="monitor backgroud in dark mode" />
+    </div>
+
   )
 }
 
