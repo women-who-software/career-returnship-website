2a16f9d35edea766e3809bdb6e7be75885ed97be
Rafna <rafnabash@gmail.com>
2020-07-24 20:32:32 -0600

    Updated the icon on the plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon-color.png`, // This path is relative to the root of the site.
+        icon: `src/images/Icon-color.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
