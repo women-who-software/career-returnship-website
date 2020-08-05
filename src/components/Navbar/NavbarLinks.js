bf2c20a486bacbc5f252d01408ded7a8881c4c40
C-Gaila <christine.d.gaila@gmail.com>
2020-08-05 12:30:31 -0600

    NavItem points to appropriate pages so far.

--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -41,12 +41,12 @@ const NavItem = Styled(Link)`
 const NavbarLinks = () => {
   return (
     <>
-      <NavItem to="/">About Us</NavItem>
-      <NavItem to="/">Networking</NavItem>
+      <NavItem to="/about">About Us</NavItem>
+      <NavItem to="/announcements">Networking</NavItem>
       <NavItem to="/">Sponsors</NavItem>
-      <NavItem to="/404">Contact Us</NavItem>
-      <NavItem to="/404">Become a Member</NavItem>
-      <NavItem to="/">Projects</NavItem>
+      <NavItem to="/contactUs">Contact Us</NavItem>
+      <NavItem to="/contactUs">Become a Member</NavItem>
+      <NavItem to="/projects">Projects</NavItem>
     </>
   )
 }
