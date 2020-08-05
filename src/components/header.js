1a208f4bffbcbe741165103353f787103b5c5155
C-Gaila <christine.d.gaila@gmail.com>
2020-08-05 09:13:25 -0600

    Deleted H1 and header background.

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `white`,
-      height: `0vh`,
+       height: `0vh`,
     }}
   >
     <div
@@ -16,17 +15,6 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
     </div>
   </header>
 )
