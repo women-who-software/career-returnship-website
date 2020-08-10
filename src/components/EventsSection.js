742513c8db5b731eef8671c26a6014d9695e616d
Finn Thye <piqueen@gmail.com>
2020-08-10 07:14:32 -0600

    Events HTML updated for flex responsiveness

--- src/components/EventsSection.js
+++ src/components/EventsSection.js
@@ -5,17 +5,17 @@ import meetupInfo from "../images/tempMeetupInfo.png"
 
 const Events = () => (
   <EventsWrapper>
-    <div className="events">
+    <div className="content">
       <h2>NETWORKING / EVENTS</h2>
-      <div className="columns">
-        <img className="meetup-info" src={meetupInfo} alt="Meetup logo with event information"/>
-        <div className="column1">
-          <EventBlurb />
+      <div className="row">
+        <div className="column">
+          <img className="meetup-info" src={meetupInfo} alt="Meetup logo with event information"/>
+        </div>
+        <div className="column">
           <EventBlurb />
           <EventBlurb />
         </div>
-        <div className="column2">
-          <EventBlurb />  
+        <div className="column">
           <EventBlurb />
           <EventBlurb />
         </div>
@@ -26,51 +26,48 @@ const Events = () => (
 )
 
 const EventsWrapper = Styled.div`
-  @media (max-width: 700px) {
+  align-items: center;
+  background: gray;
+  color: #54BBCE;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media screen and (max-width: 900px) {
 
   }
 
-  .events {
-    height: 60vh;
-    width: 100%;
-    background: gray;
-    padding: 20px 300px;
-    color: #54BBCE;
+  .columns {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
 
-    .columns {
-      display: flex;
-      flex-direction: row;
-      width: 100%;
-      justify-content: space-between;
-
-      img {
-        border-radius: 15px;
-        min-width: 280px;
-        margin-right: 30px;
-      }
-
-      .column1 {
-        min-width: 280px;
-        margin-right: 30px;
-      }
-
-      .column2 {
-        min-width: 280px;
-      }
+    img {
+      border-radius: 15px;
+      min-width: 280px;
+      margin-right: 30px;
     }
-    .link-more {
-      text-decoration: none;
-      font-family: Montserrat, sans-serif;
-      font-weight: bold;
-      font-size: 14px;
-      line-height: 123.4%;
-      letter-spacing: 0.02em;
-      color: #FFFFFF;
-      align-self: center;
-      margin-top: 20px;
+
+    .column1 {
+      min-width: 280px;
+      margin-right: 30px;
     }
+
+    .column2 {
+      min-width: 280px;
+    }
+  }
+  .link-more {
+    text-decoration: none;
+    font-family: Montserrat, sans-serif;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 123.4%;
+    letter-spacing: 0.02em;
+    color: #FFFFFF;
+    align-self: center;
+    margin-top: 20px;
   }
 `
 
