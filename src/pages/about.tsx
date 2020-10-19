ad8a0167334a90a25172604f1cc123150db8837d
Finn Thye <piqueen@gmail.com>
2020-10-19 16:58:50 -0600

    Skeleton for styles

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,31 +8,29 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <Wrapper>
-      <div classname="description">
-        <div classname="intro">
-          <div classname="title">ABOUT US</div>
-          <div classname="text">
-            Kickstarting your career and reentering the workforce after a break
-            can be a huge challenge; together we can make this a lot easier.
-            This initiative is a small step towards empowering those of us in
-            technology trying to relaunch our careers. <br />
-            Join us in this biweekly event series where we learn, network,
-            engage in meaningful projects and crack the interviews together.
-            <br />
-            Who should attend: If you are thinking about restarting your career
-            or if you are currently struggling alone in the process, this
-            program is for you. If you are currently working, we would love your
-            insights to help the community.
-          </div>
-          <button className="become-member-button" type="button">
-            BECOME A MEMBER
-          </button>
-          <button className="become-sponsor-button" type="button">
-            BE A SPONSOR/VOLUNTEER
-          </button>
+      <div className="description">
+        <div className="title">ABOUT US</div>
+        <div className="text">
+          Kickstarting your career and reentering the workforce after a break
+          can be a huge challenge; together we can make this a lot easier. This
+          initiative is a small step towards empowering those of us in
+          technology trying to relaunch our careers. <br />
+          Join us in this biweekly event series where we learn, network, engage
+          in meaningful projects and crack the interviews together.
+          <br />
+          Who should attend: If you are thinking about restarting your career or
+          if you are currently struggling alone in the process, this program is
+          for you. If you are currently working, we would love your insights to
+          help the community.
         </div>
+        <button className="become-member-button" type="button">
+          BECOME A MEMBER
+        </button>
+        <button className="become-sponsor-button" type="button">
+          BE A SPONSOR/VOLUNTEER
+        </button>
       </div>
-      <div classname="links">Hello World</div>
+      <div className="links">Hello World</div>
     </Wrapper>
   </Layout>
 )
@@ -42,6 +40,30 @@ align-items: center;
 display: flex;
 flex-direction: column;
 width: 100%;
+
+.description {
+  .title {
+    color: #DA0D46; 
+    font-weight: 600;
+    font-size: 2.4rem;
+  }
+
+  .text {
+    
+  }
+
+  .become-member-button {
+
+  }
+
+  .become-sponsor-button {
+
+  }
+}
+
+.links {
+
+}
 `
 
 export default AboutPage
