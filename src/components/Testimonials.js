d2a56d6bf8a47699fb282e1ae8ebb41165b77d02
Finn Thye <piqueen@gmail.com>
2021-02-01 13:53:18 -0700

    Styled text and widened it

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,10 +8,19 @@ import happy from "../images/temp-happyWomenCoding.jpg";
 
 export default function Slider() {
   return (
-    <AliceCarousel autoPlay autoPlayInterval="3000">
+    <AliceCarousel autoPlay infinite autoPlayInterval="5000">
         <Image src={bed} className="sliderimg"/>
+        <Text>
+          <p className="quote">"Working with the Career Returnshippers was a pleasure.  Their work was prompt and precisely to specifications."</p>
+          <br/>
+          <p className="source">~Count Dracula</p>
+        </Text>
         <Image src={happy} className="sliderimg"/>
-        <Text>Hello World</Text>
+        <Text>
+          <p className="quote">"In the Returnship, I collaborated with a team of developers and got real-world experience I couldn't get any other way."</p>
+          <br/>
+          <p className="source">~Finn Thye, volunteer</p>
+        </Text>
       {/* <img src={image3} className="sliderimg"/>
       <img src={image4} className="sliderimg"/> */}
     </AliceCarousel>
@@ -24,11 +33,15 @@ const Image = Styled.img`
 `
 
 const Text = Styled.div`
-  width: 50%;
-  background: purple;
-  margin: 1em 0 0 25%;
+  width: 75%;
+  height: 500px;
+  background: white;
+  margin: 1em 0 0 14%;
   text-align: center;
-  font-size: 4em;
+  font-size: 3em;
+  font-family: Myriad Pro;
+  border: 5px solid #DA0D46;
+  padding: 1em;
 `
 /* 
 style={{width: "50%", height: "auto", margin: "0 auto"}}
